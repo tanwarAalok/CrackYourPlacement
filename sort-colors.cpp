70fcf2c72b7e70a50c9ec21cbfe5cf45d1600d70
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-07-02 15:52:00 +0530

    Create sort-colors.cpp

---
+++ sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+
+        int curr = 0;
+
+        while(curr <= r){
+            switch(nums[curr]){
+                case 0:
+                    swap(nums[curr++], nums[l++]);
+                    break;
+                case 1: 
+                    curr++;
+                    break;
+                case 2: 
+                    swap(nums[curr], nums[r--]);
+                    break;
+            }
+        }
+    }
+};
